2b77a2bec52a6289760caca914caa4503740ea7d
abdulLaunchGood <abdul.haris@launchgood.com>
2025-10-29 15:26:55 +0900

    update code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
 // src/main.rs
-use apitap::{
-    errors::{self, Result},
-    http::{
-        fetcher::{DataFusionPageWriter, PaginatedFetcher}, Http
-    },
-    pipeline::Config,
-    utils::datafusion_ext::{DataFrameExt, JsonValueExt},
-    writer::{postgres::PostgresWriter, WriteMode},
-};
-
-use sqlx::PgPool;
-use std::fs::File;
-use std::{path::Path, sync::Arc};
+use apitap::errors::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
